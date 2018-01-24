e2d713ccfe68cf18a93d633b1e71218e737ad880
dadlerj <daniel.neal.adler@gmail.com>
2018-01-23 16:43:38 -0800

    Add setTrackUrls handler to update URL tracking

--- src/js/tracker.js
+++ src/js/tracker.js
@@ -1527,6 +1527,16 @@
 			},
 
 			/**
+			 *
+			 * Specify whether to track URLs.
+			 *
+			 * @param bool trackUrls Whether to track URLs
+			 */
+			setTrackUrls: function (trackUrls) {
+				configTrackUrls = trackUrls || false;
+			},
+
+			/**
 			* Specify the platform
 			*
 			* @param string platform Overrides the default tracking platform
